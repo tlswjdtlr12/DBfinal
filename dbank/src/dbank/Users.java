29ec9143051230fa17318a927be6f088a2e13f95
2014004739 <2014004739@hanyang.ac.kr>
2018-12-01 20:53:38 +0900

    계정 중복없이 생성완료

--- dbank/src/dbank/Users.java
+++ dbank/src/dbank/Users.java
@@ -3,25 +3,53 @@ package dbank;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Calendar;
+import java.util.Objects;
 import java.util.Random;
 
 public class Users {
 
-	public void run() {
-		String cmd;
-		String password;
-		String start_day;
-		String acuid; // 계정 번호
-		String Anumber; // 계좌 번호
-		String name;
-		String address;
-		String phone;
-		String birth; // yyyy-mm-dd 형식
-		String bnumber; // branch number (예시 주어주고 고르라고 해야함)
-		Boolean check_perfect; // 전원 나갔을 때를 대비 
+	Random rand = new Random();
+	ResultSet rset;
+	boolean same_account;
+	String acuid; // 계정 번호
+	String cmd;
+	String password;
+	String start_day;
+	String Anumber; // 계좌 번호
+	String name;
+	String address;
+	String phone;
+	String birth; // yyyy-mm-dd 형식
+	String bnumber; // branch number (예시 주어주고 고르라고 해야함)
+	Boolean check_perfect; // 전원 나갔을 때를 대비 
 
-		Random rand = new Random();
-		ResultSet rset;
+	public String check_account(String acuid) { // 중복 확인
+		try {
+			same_account = false;
+			while(rset.next()) {
+				if(Objects.equals(rset.getString("UID"), acuid)) {
+					System.out.println("같은 번호의 계정입니다. 다시 수행합니다.");
+					same_account = true;
+					acuid = "123-"+(rand.nextInt(8999)+1000)+"-"+(rand.nextInt(899)+100);
+					break;
+				}
+			}
+			if(same_account) return acuid;
+			System.out.println("계정 번호 확정");
+			return acuid;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return acuid;
+	}
+	
+	public void run() {
+		
+
+		Calendar cal = Calendar.getInstance();
+		start_day = cal.get(Calendar.YEAR)+"-"+cal.get(Calendar.MONTH)+"-"+cal.get(Calendar.DATE);				
+
+		
 		try {
 			while (true) {
 				System.out.println("어서오십시오. 작업을 선택해 주십시오.");
@@ -40,47 +68,33 @@ public class Users {
 					check_perfect = false;
 					System.out.println("* 계정 생성 *");
 
-					Calendar cal = Calendar.getInstance();
-					start_day = cal.get(Calendar.YEAR)+"-"+cal.get(Calendar.MONTH)+"-"+cal.get(Calendar.DATE);				
 
 					System.out.println(">> Type your password");
 					password = Main.sc.nextLine();
 
 
-					// 계정 번호 생성
-					int year = cal.get(Calendar.YEAR) - 2000; // 18 얘는 계좌번호
-					while(true) {
-						//계좌번호생성acuid = Integer.toString(year)+"-"+(rand.nextInt(8999)+1000)+"-"+(rand.nextInt(8999)+1000);
-						acuid = "123-"+(rand.nextInt(8999)+1000)+"-"+(rand.nextInt(899)+100);
-						System.out.println(acuid);
-						//rset = Main.stmt.executeQuery("select * from user where UID='" + acuid + "'");
-						rset = Main.stmt.executeQuery("select * from user");
-						if(rset.next())
-						{
-							if(rset.getString("UID")==acuid)
-							{
-								System.out.println("있음");
-							}
-							else System.out.println("없음");
-						}
-						else System.out.println("에러어어어");
-						break;
-					}
+					// 완료. 계정 번호 생성
+					rset = Main.stmt.executeQuery("select * from user");
+					acuid = "123-4742-957";
+					do{
+						acuid = check_account(acuid);
+					} while(same_account);
 
-					// db에서 계정 중복 있는지 확인해야함
+					
+					System.out.println("계정 : " + acuid);
 
 					System.out.println(">> Type your name");
 					name = Main.sc.nextLine();
 					System.out.println(">> Type your address");
 					address = Main.sc.nextLine();
-					System.out.println(">> Type your phone number");
+					System.out.println(">> Type your phone number (doesn't typing '-')");
 					phone = Main.sc.nextLine();
 					System.out.println(">> Type your Birth Date (YYYY-MM-DD)");
 					birth = Main.sc.nextLine();
 					System.out.println(">> Select area you want to deposit");
 					bnumber = Main.sc.nextLine();
 					// 기본입력 끝
-					// select 
+					
 
 
 					check_perfect = true;
@@ -97,6 +111,13 @@ public class Users {
 					break;
 				case "3":
 					System.out.println("* 계좌 생성 *");
+					
+
+					int year = cal.get(Calendar.YEAR) - 2000; // 18 얘는 계좌번호
+					
+					
+					
+					
 					break;
 				case "4":
 					System.out.println("* 계좌 삭제 *");
@@ -115,6 +136,6 @@ public class Users {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 }
