c6720956738b67d6d1588958a0e6f5869cbabe06
2014004739 <2014004739@hanyang.ac.kr>
2018-12-01 18:36:52 +0900

    계정생성 및 확인 작업중

--- dbank/src/dbank/Main.java
+++ dbank/src/dbank/Main.java
@@ -1,70 +1,72 @@
 package dbank;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
 	public static Scanner sc;
-    public static void main(String[] args){
-    	// project에 builds 지정 필수
-        //String url ="jdbc:mysql://127.0.0.1:3306/company";
-    	String url ="jdbc:mariadb://127.0.0.1:3306/company";
-        String user="tlswjdtlr1";
-        String password ="showme12";  
-             
-        //Driver load.    
-        try{
-        Class.forName("org.mariadb.jdbc.Driver");          
-        }catch(ClassNotFoundException e){
-            System.out.println("드라이브 설정을 찾을 수 없습니다.");
-        }
-        
-        //Connection.
-        Connection conn = null;
-        try {
-             conn = DriverManager.getConnection(url,user,password);
-            //--==>> Connection 개체를 반환한다.
-        } catch (SQLException e) {
-            System.out.println("DB접속 중 오류가 발생하였습니다.");
-            e.printStackTrace();
-        }
-        
-        sc = new Scanner(System.in);
-        //SQL
-        StringBuilder sqlBuilder = new StringBuilder();
-        sqlBuilder.append("SELECT * ");
-        sqlBuilder.append(" FROM document ");
-        
-        //--==>> Database에서 수행 가능한 객체로 변환시켜주는 구문.
-        Statement stmt =null;
-        ResultSet rs = null; //--==>>sun에서 제공하는 API 중 하나
-                             //--==>>커서가 이동한 후 데이터의 유/무를 판단해서 true/false 반환
-        try {
-            stmt = conn.createStatement();
-            
-            //SQL execute
-            rs = stmt.executeQuery(sqlBuilder.toString());
-            //--==>>위에서 정의한 String을 실행
-            while(rs.next()){
-                //String empNo = rs.getString("empNo"); // =  rs.getString(1); : 첫 번째 데이터 주세요
-                                                      //--==>> 컬럼명을 직접 명시해 주는 것이 더 낫다. (확인 불필요 등등...)
-               
-                //int salary = rs.getInt("salary");
-            	String Fname = rs.getString("registerday");
-            	String Minit = rs.getString("Dname");
-            	String Lname = rs.getString("Daddr");
-            	System.out.println(Fname + " " + Minit + " " + Lname);
-            }
-        } catch (SQLException e) {
-            System.out.println("SQL 수행중 오류가 발생했습니다.");
-        }
-              
-        // xor암호 : https://blog.naver.com/zkd1750/90192019205
-        /* 
+	public static Statement stmt;
+	public static void main(String[] args){
+		// project에 builds 지정 필수
+		//String url ="jdbc:mysql://127.0.0.1:3306/company";
+		String url ="jdbc:mariadb://127.0.0.1:3306/company";
+		String user="tlswjdtlr1";
+		String password ="showme12";  
+
+		//Driver load.    
+		try{
+			Class.forName("org.mariadb.jdbc.Driver");          
+		}catch(ClassNotFoundException e){
+			System.out.println("드라이브 설정을 찾을 수 없습니다.");
+		}
+
+		//Connection.
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url,user,password);
+			//--==>> Connection 개체를 반환한다.
+
+			stmt = conn.createStatement();
+			sc = new Scanner(System.in);
+			Users users = new Users();
+			users.run();
+			//SQL
+		} catch (SQLException e) {
+			System.out.println("DB접속 중 오류가 발생하였습니다.");
+			e.printStackTrace();
+		}
+		StringBuilder sqlBuilder = new StringBuilder();
+		sqlBuilder.append("SELECT * ");
+		sqlBuilder.append(" FROM document ");
+
+		//--==>> Database에서 수행 가능한 객체로 변환시켜주는 구문.
+		//Statement stmt =null;
+		ResultSet rs = null; //--==>>sun에서 제공하는 API 중 하나
+		//--==>>커서가 이동한 후 데이터의 유/무를 판단해서 true/false 반환
+		try {
+			//stmt = conn.createStatement();
+
+			//SQL execute
+			rs = stmt.executeQuery(sqlBuilder.toString());
+			//--==>>위에서 정의한 String을 실행
+			while(rs.next()){
+				//String empNo = rs.getString("empNo"); // =  rs.getString(1); : 첫 번째 데이터 주세요
+				//--==>> 컬럼명을 직접 명시해 주는 것이 더 낫다. (확인 불필요 등등...)
+
+				//int salary = rs.getInt("salary");
+				String Fname = rs.getString("registerday");
+				String Minit = rs.getString("Dname");
+				String Lname = rs.getString("Daddr");
+				System.out.println(Fname + " " + Minit + " " + Lname);
+			}
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println("SQL 수행중 오류가 발생했습니다.");
+		}
+
+		// xor암호 : https://blog.naver.com/zkd1750/90192019205
+		/* 
           계좌번호 생성
 
 nAccount="110-499-"+(rand.nextInt(500000)+100000);
@@ -78,6 +80,6 @@ nAccount="110-499-"+(rand.nextInt(500000)+100000);
 					catch(Exception e) {}
 
 
-*/
-    }
+		 */
+	}
 }
--- dbank/src/dbank/Users.java
+++ dbank/src/dbank/Users.java
@@ -1,6 +1,9 @@
 package dbank;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Calendar;
+import java.util.Random;
 
 public class Users {
 
@@ -14,57 +17,104 @@ public class Users {
 		String address;
 		String phone;
 		String birth; // yyyy-mm-dd 형식
-		int bnumber; // branch number (예시 주어주고 고르라고 해야함)
+		String bnumber; // branch number (예시 주어주고 고르라고 해야함)
 		Boolean check_perfect; // 전원 나갔을 때를 대비 
-		while (true) {
-			System.out.println("어서오십시오. 작업을 선택해 주십시오.");
-			System.out.println("1. 계정 생성");
-			System.out.println("2. 계정 삭제");
-			System.out.println("3. 계좌 생성");
-			System.out.println("4. 계좌 삭제");
-			System.out.println("5. 잔액 조회");
-			System.out.println("6. 입금");
-			System.out.println("7. 출금");
-			String choice = Main.sc.nextLine();
 
-			switch (choice){
-			case "1":
-				check_perfect = false;
-				System.out.println("* 계정 생성 *");
-				System.out.println(">> Type your password");
-				password = Main.sc.nextLine();
-				start_day = "2018-11-30";
-				// acuid = "18-1223-5123"
-				
-				Calendar cal = Calendar.getInstance();
-		        int year = cal.get(Calendar.YEAR) - 2000; // 18
-		        // 계정 번호 생성
-		        // db에서 계정 중복 있는지 확인해야함
-				
-		        System.out.println(">> Type your name");
-		        
-		        
-				check_perfect = true;
-				break;
-			case "2":
-				System.out.println("* 계정 삭제 *");
-				break;
-			case "3":
-				System.out.println("* 계좌 생성 *");
-				break;
-			case "4":
-				System.out.println("* 계좌 삭제 *");
-				break;
-			case "5":
-				System.out.println("* 잔액 조회 *");
-				break;
-			case "6":
-				System.out.println("* 입금 *");
-				break;
-			case "7":
-				System.out.println("* 출금 *");
-				break;
+		Random rand = new Random();
+		ResultSet rset;
+		try {
+			while (true) {
+				System.out.println("어서오십시오. 작업을 선택해 주십시오.");
+				System.out.println("1. 계정 생성");
+				System.out.println("2. 계정 삭제");
+				System.out.println("3. 계좌 생성");
+				System.out.println("4. 계좌 삭제");
+				System.out.println("5. 잔액 조회");
+				System.out.println("6. 입금");
+				System.out.println("7. 출금");
+				System.out.print("your choice : ");
+				String choice = Main.sc.nextLine();
+
+				switch (choice){
+				case "1":
+					check_perfect = false;
+					System.out.println("* 계정 생성 *");
+
+					Calendar cal = Calendar.getInstance();
+					start_day = cal.get(Calendar.YEAR)+"-"+cal.get(Calendar.MONTH)+"-"+cal.get(Calendar.DATE);				
+
+					System.out.println(">> Type your password");
+					password = Main.sc.nextLine();
+
+
+					// 계정 번호 생성
+					int year = cal.get(Calendar.YEAR) - 2000; // 18 얘는 계좌번호
+					while(true) {
+						//계좌번호생성acuid = Integer.toString(year)+"-"+(rand.nextInt(8999)+1000)+"-"+(rand.nextInt(8999)+1000);
+						acuid = "123-"+(rand.nextInt(8999)+1000)+"-"+(rand.nextInt(899)+100);
+						System.out.println(acuid);
+						//rset = Main.stmt.executeQuery("select * from user where UID='" + acuid + "'");
+						rset = Main.stmt.executeQuery("select * from user");
+						if(rset.next())
+						{
+							if(rset.getString("UID")==acuid)
+							{
+								System.out.println("있음");
+							}
+							else System.out.println("없음");
+						}
+						else System.out.println("에러어어어");
+						break;
+					}
+
+					// db에서 계정 중복 있는지 확인해야함
+
+					System.out.println(">> Type your name");
+					name = Main.sc.nextLine();
+					System.out.println(">> Type your address");
+					address = Main.sc.nextLine();
+					System.out.println(">> Type your phone number");
+					phone = Main.sc.nextLine();
+					System.out.println(">> Type your Birth Date (YYYY-MM-DD)");
+					birth = Main.sc.nextLine();
+					System.out.println(">> Select area you want to deposit");
+					bnumber = Main.sc.nextLine();
+					// 기본입력 끝
+					// select 
+
+
+					check_perfect = true;
+					break;
+				case "2":
+					System.out.println("* 계정 삭제 *");
+
+					// 계정 번호 입력
+					// 비밀번호 입력
+					// 확인
+					// 삭제
+					// 도큐먼트 수정
+
+					break;
+				case "3":
+					System.out.println("* 계좌 생성 *");
+					break;
+				case "4":
+					System.out.println("* 계좌 삭제 *");
+					break;
+				case "5":
+					System.out.println("* 잔액 조회 *");
+					break;
+				case "6":
+					System.out.println("* 입금 *");
+					break;
+				case "7":
+					System.out.println("* 출금 *");
+					break;
+				}
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+		
 	}
 }
